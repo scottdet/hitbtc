cf8e23a37f7621f92b0a8aeec53ce07ec53a5f38
scottdet <scottdetaboada@g.ucla.edu>
2017-11-15 13:56:50 -0800

    Update hitbtc_wrapper.py

--- hitbtc_wrapper.py
+++ hitbtc_wrapper.py
@@ -5,69 +5,43 @@ import time, csv, os
 import numpy as np
 import requests
 
-auth = ("public", "secret")
+# ------------------------------ #
+#       HitBtc Functions         #
+# ------------------------------ #
 
-# list of portfolio assets
-pairs = ['BCCBTC', 'BCCETH', 'ETHBTC']
-data = []
+class hitbtc(object):
 
-# for public methods, no need to pass (key, value) pairs or auth
-def query_public(method):
-	url = 'https://api.hitbtc.com/api/2/public/'
-	url += method
-	req = requests.get(url)
-	return req.json()
+    # initialize things
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = secret
 
-# query methods that have (key, value) pairs, pretty much just order
-def query(method, values={}):
-	url = 'https://api.hitbtc.com/api/2/'
-	url += method
-	req = requests.post(url, data=values, auth=auth)
-	return req.json()
+    # for public methods, no need to pass (key, value) pairs or auth
+    def query_public_hitbtc(self, method):
+        url = 'https://api.hitbtc.com/api/2/public/'
+        url += method
+        req = requests.get(url)
+        return req.json()
 
-# get pricing data for specific currency pair
-def get_ticker(symbol):
-	return query_public('ticker/%s' % symbol)
+    # query methods that have (key, value) pairs, pretty much just order
+    def query_hitbtc(self, method, values={}):
+        url = 'https://api.hitbtc.com/api/2/'
+        url += method
+        req = requests.post(url, data=values, auth=(self.key, self.secret))
+        return req.json()
 
-# get all balances
-# TODO: update so this only returns currencies with non-zero balance
-def get_balances():
-	url = 'https://api.hitbtc.com/api/2/trading/balance'
-	req = requests.get(url, auth=auth)
-	return req.json()
+    # get pricing data for specific currency pair
+    def get_ticker_hitbtc(self, symbol):
+        return self.query_public_hitbtc('ticker/%s' % symbol)
 
-# place a new order, includes "fill or kill" and "immediate or cancel" options
-# fees of 0.1% as noted in hitbtc docs
-def new_order(symbol, side, quantity, price, timeInForce):
-	return query('order', {'symbol': symbol, 'side': side, 'quantity': quantity, 'price': price, 'timeInForce': timeInForce})
- 
-# --------------- #
-#    main loop    #
-# --------------- #
+    # get all balances
+    # TODO: update so this only returns currencies with non-zero balance
+    def get_balances_hitbtc(self):
+        url = 'https://api.hitbtc.com/api/2/trading/balance'
+        req = requests.get(url, auth=(self.key, self.secret))
+        return req.json()
 
-# random example for how to use some of the above functions
-# see api docs to implement other functions or to see what paramaters are available 
-if __name__ == "__main__":
-	while(True):
-		# append the dater
-		date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-		data.append(date)
-
-		# loop to get available balances as an example
-		balances = get_balances()
-		for i in range(len(balances)):
-			if balances[i]['currency'] == 'BTC':
-				x1 = balances[i]['available']
-			if balances[i]['currency'] == 'ETH':
-				y1 = balances[i]['available']
-			if balances[i]['currency'] == 'OMG':
-				z1 = balances[i]['available']
-
-		# loop to get data for all currency pairs
-		for j in range(len(pairs)):
-			summaries = get_ticker(pairs[j])
-			bid = float(summaries['bid'])
-			ask = float(summaries['ask'])
-
-			data.append(bid)
-			data.append(ask)
+    # place a new order, includes "fill or kill" and "immediate or cancel" options
+    # fees of 0.1% as noted in hitbtc docs
+    def new_order_hitbtc(self, symbol, side, quantity, price, timeInForce):
+        return self.query_hitbtc('order', {'symbol': symbol, 'side': side, 'quantity': quantity, 'price': price, 'timeInForce': timeInForce})
